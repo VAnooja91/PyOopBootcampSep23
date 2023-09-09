9d6c0d32e1ea02685c6af9fa07a4ecb9661608e8
Anooja <76803743+VAnooja91@users.noreply.github.com>
2023-09-09 15:07:43 +0530

    using super keyword for reuse parent class methods

--- Effective_Python/Work/child.py
+++ Effective_Python/Work/child.py
@@ -5,7 +5,8 @@ class GSTProduct(Product):
         self.sell(self.quant)
 
     def cost(self):
-        return 1.25*self.quant *self.price
+        actual_cost = super().cost()
+        return 1.25*actual_cost
 
 c = GSTProduct("Mint",100,210.1) #child class obj
 print(c.cost())
