8b3457c76da78472006201a05eafb5df9096931b
Anooja <76803743+VAnooja91@users.noreply.github.com>
2023-09-09 11:37:13 +0530

    First commit

---
+++ Effective_Python/Work/shop.py
@@ -0,0 +1,14 @@
+from report import read_inventory
+
+
+def recipt(filename):
+    inv = read_inventory(filename)
+    total = 0
+    for pr in inv:
+        total += pr['quant'] * pr['price']
+    
+    return total
+
+amount = recipt("Data/inventory.csv")
+
+print(f"your bill amount is : {amount}")
